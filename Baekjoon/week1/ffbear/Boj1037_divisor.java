0a26b15cb2ceeebe25be027e901a46134e8f3d43
ffbear <kth6699@naver.com>
2022-02-08 00:00:20 +0900

    백준 코딩테스트 실버 - Boj 1037 약수

---
+++ Baekjoon/week1/ffbear/Boj1037_divisor.java
@@ -0,0 +1,32 @@
+package Baekjoon.week1.ffbear;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Boj1037_divisor {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] input = br.readLine().split(" ");
+        int[] nums = new int[input.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+
+
+        Arrays.sort(nums);
+
+
+        bw.append(String.valueOf(nums[nums.length - 1] * nums[0]));
+        bw.flush();
+        bw.close();
+
+    }
+
+}
